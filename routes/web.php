f359d10027a64f69f7654509846ca2def7e05e8d
Zoran Jankovic <zjkiza@gmail.com>
2019-04-21 13:30:20 +0200

    create route for Product

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', 'ProductController@index')->name('product.index');
+Route::get('/products/create', 'ProductController@create')->name('product.create');
+Route::get('/products/{product}', 'ProductController@show')->name('product.show');
+Route::get('/products/{product}/edit', 'ProductController@edit')->name('product.edit');
+Route::post('/products/{product}', 'ProductController@store')->name('product.store');
+Route::delete('/products/{product}', 'ProductController@destroy')->name('product.destroy');
+Route::patch('/products/{product}', 'ProductController@update')->name('product.update');
