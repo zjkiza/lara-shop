c2b706c382c451536e2523a35cf7252338382421
Zoran Jankovic <zjkiza@gmail.com>
2019-05-17 14:25:00 +0200

    fix code style

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -9,12 +9,12 @@ class ProductObserver
     /**
      * Handle the product "updated" event.
      *
-     * @param  Product  $product
+     * @param  Product $product
      * @return void
      */
     public function updated(Product $product): void
     {
-        if((int)$product->quantity === 0 && $product->status !== 'old') {
+        if ((int)$product->quantity === 0 && $product->status !== 'old') {
             $product->status = 'old';
             $product->save();
         }
